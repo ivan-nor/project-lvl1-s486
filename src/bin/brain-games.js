2382a7662cab89a5182c38f862e80806d0c8093b
Ivan Nor <ivan.norik@gmail.com>
2019-05-03 20:31:49 +0300

    add brain-even

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import sayHi from '../index';
-
-console.log('Welcome to the Brain Games!');
+import { sayHi } from '../index';
 
 sayHi();
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import readlineSync from 'readline-sync';
 const sayHi = () => {
   const userName = readlineSync.question('May I have your name, please? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-export default sayHi;
+const askEven = () => {
+  const userAnswer = readlineSync.question('Your answer: ');
+  return userAnswer;
+};
+
+const welcome = () => {
+  console.log('Welcome to the Brain Games!', 'Answer "yes" if number even otherwise answer "no"');
+};
+
+export { sayHi, askEven, welcome };
