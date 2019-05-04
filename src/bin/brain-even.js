73cb4bea4dbea93f8b3ad9f89f0bbc7033c95959
Ivan Nor <ivan.norik@gmail.com>
2019-05-04 13:06:13 +0300

    add logic to brain-even.js

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,39 @@
 #!/usr/bin/env node
-import { welcome, sayHi } from '../index';
+import {
+  welcome, askName, getRandomNumber, congratulations, oops, endGame, getEven,
+  conditionsGamesEven,
+  askQuestion, equalAnswer, askAnswer, correctAnswer,
+} from '../index';
 
-welcome();
-sayHi();
+const start = () => {
+  const game = () => {
+    return {
+      randomNumber: getRandomNumber(),
+      isEven: getEven(this.randomNumber),
+      askQuestion: askQuestion(this.randomNumber),
+      userAnswer: askAnswer(),
+      equalAnswer: equalAnswer(this.userAnswer, this.isEven),
+    };
+  };
+
+  welcome();
+  conditionsGamesEven();
+  const userName = askName();
+
+  for (let i = 1; i <= 3; i += 1) {
+    const resultGame = game();
+    if (resultGame.equalAnswer) {
+      correctAnswer();
+    } else {
+      oops(userName, resultGame.userAnswer, resultGame.isEven);
+      endGame();
+    }
+  }
+
+
+  congratulations(userName);
+
+  endGame();
+};
+
+start();
