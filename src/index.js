2cf9b04739bbabe11592573275814a1b76794e96
Ivan Nor <ivan.norik@gmail.com>
2019-05-06 14:36:55 +0300

    add function getRandomOperations and fucntion oops and fixed export

--- src/index.js
+++ src/index.js
@@ -35,7 +35,33 @@ const equalAnswer = (userAnswer, isTrue) => userAnswer === isTrue;
 
 const correctAnswer = () => console.log('Correct!');
 
+const getRandomOperation = (begin, end) => {
+  const randomOperation = getRandomNumber(begin, end);
+  let result = '';
+  switch (randomOperation) {
+    case 1:
+      result = '+';
+      break;
+    case 2:
+      result = '-';
+      break;
+    case 3:
+      result = '*';
+      break;
+    case 4:
+      result = '/';
+      break;
+    default:
+      console.log('default');
+  }
+  return result;
+};
+
+const oops = (userName, userAnswer, isRight) => console.log(
+  `'${userAnswer}' is wrong answer ;(. Correct answer was '${isRight}'. Let's try again, ${userName}!`,
+);
+
 export {
-  askName, askAnswer, welcome, getRandomNumber, congratulations,
-  endGame, askQuestion, equalAnswer, correctAnswer,
+  askName, askAnswer, welcome, getRandomNumber, congratulations, oops,
+  endGame, askQuestion, equalAnswer, correctAnswer, getRandomOperation,
 };
