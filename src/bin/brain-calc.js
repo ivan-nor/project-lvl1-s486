61ccd9678f11d9b820441a485fb9ce43584ab109
Ivan Nor <ivan.norik@gmail.com>
2019-05-06 14:38:21 +0300

    add logic of game brain-calc

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,13 +1,44 @@
 #!/usr/bin/env node
 
-import { welcome, askName } from '..';
+import {
+  welcome, askName, getRandomNumber, congratulations,
+  endGame, correctAnswer, askAnswer, askQuestion, equalAnswer,
+  getRandomOperation, oops,
+} from '../index';
 
 const conditionsGamesCalc = () => console.log('What is the result of the expression?');
+
+const getStringCalc = (first, second, operation) => `${first}${operation}${second}`;
 
 const game = () => {
   welcome();
   conditionsGamesCalc();
-  askName();
+  const userName = askName();
+
+  for (let i = 1; i <= 3; i += 1) {
+    const resultGame = {};
+    resultGame.firstRandomNumber = getRandomNumber();
+    resultGame.secondRandomNumber = getRandomNumber();
+    resultGame.randomOperation = getRandomOperation(1, 3);
+    resultGame.stringQuestion = getStringCalc(
+      resultGame.firstRandomNumber, resultGame.secondRandomNumber, resultGame.randomOperation,
+    );
+    // eslint-disable-next-line no-eval
+    resultGame.isRight = eval(resultGame.stringQuestion);
+    resultGame.askQuestion = askQuestion(resultGame.stringQuestion);
+    resultGame.userAnswer = askAnswer();
+    resultGame.equalAnswer = equalAnswer(Number(resultGame.userAnswer), resultGame.isRight);
+    if (resultGame.equalAnswer) {
+      correctAnswer();
+    } else {
+      oops(userName, resultGame.userAnswer, resultGame.isRight);
+      endGame();
+    }
+  }
+
+  congratulations(userName);
+
+  endGame();
 };
 
 game();
