334a532cd6dfba9cf2002c0c6162a9d0bfc27eff
Ivan Nor <ivan.norik@gmail.com>
2019-05-22 20:30:50 +0300

    fixed export

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
-export const getRandomNumber = (min = 0, max = 100) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const getRandomNumber = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomOperation = (length = 4) => {
+const getRandomOperation = (length = 4) => {
   const arr = ['+', '-', '*', '/'];
   const operations = arr.slice(0, length);
   const random = getRandomNumber(0, length - 1);
   return operations[random];
 };
+
+export { getRandomNumber, getRandomOperation };
